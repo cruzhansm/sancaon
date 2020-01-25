26aa713d6979025d9e59a9f2e935904708845afc
Hans Maco Cruz <cruzhansm@gmail.com>
2020-01-25 10:02:55 +0800

    Minor changes. Click to see change log.
    Change Log
    
    Adjustments:
    1) Fixed alignment and positioning of Canteen name.
    2) Allowed tapping of canteen image. Once tapped, CLI echo is produced.
    3) Fixed missing bottom padding for Cafe+ canteen.
    
    New:
    1) Created a function that determines if the canteen is the last canteen. It then returns a different bottom padding.

--- lib/home.dart
+++ lib/home.dart
@@ -12,19 +12,19 @@ class HomeOverview extends StatelessWidget {
       'SAFAD Canteen',
       'Cafe+'];
     int newCanteenCount = 0;
-
+    final int numberOfCanteens = 6;
 
     List<Widget> listMyWidgets() {
-      List<Widget> list = new List();
+      List<Widget> listOfCanteens = new List();
       
-      while(newCanteenCount <= 5) {
-        list.add(new HomePage(
+      while(newCanteenCount <= numberOfCanteens-1) {
+        listOfCanteens.add(new HomePage(
           nameOfImage: imageName[0], 
           nameOfCanteen: canteenName[newCanteenCount]));
         newCanteenCount++;
       }
 
-      return list;
+      return listOfCanteens;
     }
 
     return ListView(
@@ -40,6 +40,18 @@ class HomePage extends StatefulWidget {
 
   HomePage({this.nameOfImage, this.nameOfCanteen});
 
+  /*
+  This function returns the padding of 20.0 if the canteen is the last canteen
+  */
+  double lastWidgetBotPadding(nameOfCanteen) {
+    if(nameOfCanteen == 'Cafe+') {
+      return 20.0;
+    }
+    else {
+      return 0.0;
+    }
+  }
+
   @override
   _HomePageState createState() => _HomePageState();
 }
@@ -50,29 +62,61 @@ class _HomePageState extends State<HomePage> {
     return Container(
       child: new Stack(
         children: <Widget>[
-          new Padding(
-            padding: EdgeInsets.fromLTRB(30.0, 20.0, 30.0, 0.0),
-            child: new ClipRRect(
-              borderRadius: new BorderRadius.circular(15.0),
-                child: Image.asset('assets/images/${widget.nameOfImage}',
-              )
-            ),
-          ),
-          new ClipRRect(
-            borderRadius: BorderRadius.circular(10.0),
-            child: new Container(
-              color: Colors.black,
-              child: Text(
-                "${widget.nameOfCanteen}",
-                style: TextStyle(
-                  fontFamily: "ABeeZee",
-                  fontSize: 20.0,
-                  color: Colors.white
+          new InkWell(
+            splashColor: Colors.yellow,
+            onLongPress: () => print("Long press option"), // optional feature!
+            onTap: () => print("Open ${widget.nameOfCanteen}"),
+            child: new Padding(
+              padding: EdgeInsets.fromLTRB(30.0, 20.0, 30.0, widget.lastWidgetBotPadding(widget.nameOfCanteen)),
+              child: new ClipRRect(
+                borderRadius: new BorderRadius.circular(15.0),
+                  child: Image.asset('assets/images/${widget.nameOfImage}',
                 ),
               ),
             ),
           ),
-        ]
+          new Column(
+            children: <Widget>[
+              new Padding(
+                padding: EdgeInsets.all(15.0),
+              ),
+              new Container(
+                width: 500.0,
+                child: new Container(
+                  alignment: Alignment(-0.66,0),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(15.0),
+                    child: new Container(
+                      constraints: BoxConstraints(
+                        maxWidth: 200.0,
+                        minWidth: 10.0,
+                      ),
+                      padding: EdgeInsets.symmetric(horizontal:5.0),
+                      color: Colors.white,
+                      child: new Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget> [
+                          Icon(
+                            Icons.location_on,
+                            color: Colors.black,
+                          ),
+                          Text(
+                            "${widget.nameOfCanteen}",
+                            style: TextStyle(
+                              fontFamily: "ABeeZee",
+                              fontSize: 15.0,
+                              color: Colors.black
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
