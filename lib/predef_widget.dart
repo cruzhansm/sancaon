2095710f0e3550df8c8259536c41d7c0298ff751
ayuna-lgc <paradoxictime@gmail.com>
2020-01-28 21:04:57 +0800

    small change, just made the sidemenu openable.

--- lib/predef_widget.dart
+++ lib/predef_widget.dart
@@ -7,10 +7,46 @@ class MainWidget extends StatefulWidget {
 }
 
 class _MainWidgetState extends State<MainWidget> {
+  GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey(); //Uayan (U): key for drawer opening purposes
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       extendBodyBehindAppBar: true, // comment if you want transparent appBar!
+      key: _scaffoldKey,            //(U): the new key
+      drawer: Drawer(                   //(U): Made a drawer
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text(
+                "Sa'nCaon",
+              style: TextStyle(
+                fontFamily: "ABeeZee",
+                fontSize: 25.0,
+                color: Colors.black
+                ),
+              ),
+            ),
+            ListTile(
+              title: Text('Settings'),
+              onTap: () => print("Open Settings"),
+            ),
+            ListTile(
+              title: Text('Feedback'),
+              onTap: () => print("Open Feedback"),
+            ),
+            ListTile(
+              title: Text('Help'),
+              onTap: () => print("Open Help"),
+            ),
+            ListTile(
+              title: Text('Credits Page'),
+              onTap: () => print("Open Credits Page"),
+            ),
+          ],
+        ),
+      ),
       appBar: new AppBar(
         elevation: 0.0,
         title: Text(
@@ -32,7 +68,9 @@ class _MainWidgetState extends State<MainWidget> {
                   Icons.menu,
                   color: Colors.black,
                 ),
-                onPressed: () => print("Menu was pressed"),
+                onPressed: () {
+                  _scaffoldKey.currentState.openDrawer(); //(U) made the 3 line icon open a drawer. Uses the global key
+                },
               ),
             )
           ],
