35f4d2a2084c0c2674c4f055a94b15dbab92e8fb
Hans Maco Cruz <cruzhansm@gmail.com>
2020-02-13 00:25:03 +0800

    Added StartUp

--- lib/startup.dart
+++ lib/startup.dart
@@ -8,16 +8,60 @@ class StartUp extends StatefulWidget {
 }
 
 class _StartUp extends State<StartUp> {
+
+  int tapCount = 0;
+
+  void changeColor(int tapCount) {
+    List<Color> textColors = [
+      Colors.green[400],
+      Colors.lightBlue[300],
+      Colors.yellow[800]
+    ];
+
+    List<String> loadingTextChange = [
+      'Loading assets',
+      'Loading images',
+      'Loading meals'
+    ];
+
+    setState(() {
+      newColor = textColors[tapCount];
+      loadText = loadingTextChange[tapCount];
+    });
+  }
+
+  Color newColor = Colors.green[400];
+  String loadText = "Loading assets";
+
   Widget build(BuildContext context) {
     return SplashScreen(
-        seconds: 9,
-        backgroundColor: Colors.white,
-        image: Image.asset('assets/images/sancaon_logo_gif.gif'),
-        loaderColor: Colors.green,
-        photoSize: 150.0,
-        navigateAfterSeconds: Home()
-        );
+      seconds: 10,
+      onClick: () => {
+        print(tapCount),
+        tapCount++,
+        changeColor(tapCount > 2? tapCount = 0: tapCount)
+      },
+      image: Image.asset('assets/images/sancaon_logo_gif.gif'),
+      loaderColor: Colors.green[400],
+      photoSize: 150.0,
+      title: new Text(
+        "Sa'nCaon",
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          fontSize: 80.0,
+          fontFamily: "Dosis",
+          fontWeight: FontWeight.bold,
+          color: newColor,
+        ),
+      ),
+      navigateAfterSeconds: Home(),
+      loadingText: new Text(
+        loadText,
+        style: TextStyle(
+          fontSize: 20.0,
+          fontFamily: "Quicksand"
+        )
+      ),
+    );
   }
 }
-
-
