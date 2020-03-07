7fdd6352e31556430a7d0dff0b01461c88c11d92
Nico夜叉 <ddumalagan91@gmail.com>
2020-03-07 13:50:19 +0800

    continue updating of menu items

--- lib/canteen_data/Bunzel_Basement/bunzelb_stalls.dart
+++ lib/canteen_data/Bunzel_Basement/bunzelb_stalls.dart
@@ -28,17 +28,6 @@ class BunzelBStalls {
         foodAllergens: null
       ),
       3 : new FoodItem(
-        foodName: "Hamsilog",
-        foodPrice: 60.00,
-        hasManyOptions: false,
-        foodImage: "assets/images/canteen-menu/BunzelCanteen/Amboy's_Food_Corner/amboys1.jpg",
-        rating: "red",
-        foodIngredients: [
-          "Pork"
-        ],
-        foodAllergens: null
-      ),
-      4 : new FoodItem(
         foodName: "Longsilog",
         foodPrice: 60.00,
         hasManyOptions: false,
@@ -49,7 +38,7 @@ class BunzelBStalls {
         ],
         foodAllergens: null
       ),
-      5 : new FoodItem(
+      4 : new FoodItem(
         foodName: "Pork Chop",
         foodPrice: 50.00,
         hasManyOptions: false,
@@ -60,7 +49,7 @@ class BunzelBStalls {
         ],
         foodAllergens: null
       ),
-      6 : new FoodItem(
+      5 : new FoodItem(
         foodName: "Sigsilog",
         foodPrice: 60.00,
         hasManyOptions: false,
@@ -73,7 +62,7 @@ class BunzelBStalls {
         ],
         foodAllergens: null
       ),
-      7 : new FoodItem(
+      6 : new FoodItem(
         foodName: "Steam Rice with Siomai",
         foodPrice: 40.00,
         hasManyOptions: false,
@@ -84,7 +73,7 @@ class BunzelBStalls {
         ],
         foodAllergens: null
       ),
-      8: new FoodItem(
+      7 : new FoodItem(
         foodName: "Tapasilog",
         foodPrice: 60.00,
         hasManyOptions: false,
@@ -95,23 +84,12 @@ class BunzelBStalls {
         ],
         foodAllergens: null
       ),
-      9: new FoodItem(
+      8 : new FoodItem(
         foodName: "Tocisilog",
         foodPrice: 60.00,
         hasManyOptions: false,
         foodImage: "assets/images/canteen-menu/BUNZELBasement/chicos8.jpg",
         rating: "yellow",
-        foodIngredients: [
-          ""
-        ],
-        foodAllergens: null
-      ),
-      10 : new FoodItem(
-        foodName: "Tunasilog",
-        foodPrice: 60.00,
-        hasManyOptions: false,
-        foodImage: "assets/images/canteen-menu/BunzelCanteen/Amboy's_Food_Corner/amboys1.jpg",
-        rating: "red",
         foodIngredients: [
           ""
         ],
